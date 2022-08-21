42dfb39a16c314cadd4fce16df106d2f8d50c0ee
knockkey <591961399@qq.com>
2022-08-22 00:51:13 +0800

    github和gitee

--- src/components/common/tabbar/TabBarItem.vue
+++ src/components/common/tabbar/TabBarItem.vue
@@ -23,68 +23,65 @@
       <slot name="item-text"></slot>
     </div>
   </div>
-
 </template>
 
 <script>
-  export default {
-    name: "TabBarItem",
-    props: {
-      //    把正在用哪个路径传进来
-      path: String,
-      activeColor: {
-        type: String,
-        default: 'red'
-      }
+export default {
+  name: "TabBarItem",
+  props: {
+    //    把正在用哪个路径传进来
+    path: String,
+    activeColor: {
+      type: String,
+      default: "red"
+    }
+  },
+  data() {
+    return {
+      // isActive: true
+    };
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) != -1;
     },
-    data() {
-      return {
-        // isActive: true
-      }
-    },
-    computed: {
-      isActive() {
-        return this.$route.path.indexOf(this.path) != -1
-      },
-      activeStyle() {
-        return this.isActive ? {color: this.activeColor} : {}
-      }
-    },
-    methods: {
-      itemClick() {
-        // console.log('监听itemClick的点击事件');
-        //所有的组件都有  this.$router    这个对象
-        //需要让item传进来 用哪个item就传哪个进来
-        //这里是手动 跳转 靠外面传东西进来，  不是用 router-link
-        this.$router.replace(this.path)
-      }
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {};
+    }
+  },
+  methods: {
+    itemClick() {
+      // console.log('监听itemClick的点击事件');
+      //所有的组件都有  this.$router    这个对象
+      //需要让item传进来 用哪个item就传哪个进来
+      //这里是手动 跳转 靠外面传东西进来，  不是用 router-link
+      this.$router.replace(this.path);
     }
   }
+};
 </script>
 
 <style scoped>
-  .tab-bar-item {
-    /*对多个flex进行均等分*/
-    flex: 1;
-    /*  水平居中文本*/
-    text-align: center;
-    /*  一般来说 tab-bar 的高度是49px*/
-    height: 49px;
-    /*  垂直居中文本*/
-    /*line-height: 49px;*/
-    font-size: 14px;
-    /*图标距离上边框3px*/
-    margin-top: 4px;
-    /*  图标和文字之间有图片默认的距离 去掉它*/
-    vertical-align: middle;
-    /*  原来图标距离文字默认3px 上一步去除了这个距离 现在重新设置距离*/
-    margin-bottom: 2px;
-  }
+.tab-bar-item {
+  /*对多个flex进行均等分*/
+  flex: 1;
+  /*  水平居中文本*/
+  text-align: center;
+  /*  一般来说 tab-bar 的高度是49px*/
+  height: 49px;
+  /*  垂直居中文本*/
+  /*line-height: 49px;*/
+  font-size: 14px;
+  /*图标距离上边框3px*/
+  margin-top: 4px;
+  /*  图标和文字之间有图片默认的距离 去掉它*/
+  vertical-align: middle;
+  /*  原来图标距离文字默认3px 上一步去除了这个距离 现在重新设置距离*/
+  margin-bottom: 2px;
+}
 
-  .tab-bar-item img {
-    width: 24px;
-    height: 24px;
-  }
-
-
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+}
 </style>
--- src/network/home.js
+++ src/network/home.js
@@ -1,15 +1,15 @@
-import {request} from "./request"
+import { request } from "./request"
 export function getHomeMultidata() {
   return request({
-    url:'/home/multidata'
+    url: '/home/multidata'
   })
 }
 
 //首页的流行新款精选
-export function getHomeGoods(type,page) {
+export function getHomeGoods(type, page) {
   return request({
-    url:'/home/data',
-    params:{
+    url: '/home/data',
+    params: {
       type,
       page
     }
